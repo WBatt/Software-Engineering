509b92c2e22e1782cbe5325671e7f697a66d3e67
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2017-02-03 23:46:16 -0800

    git ignore updated

--- Software-Engineering/main.cpp
+++ Software-Engineering/main.cpp
@@ -1,8 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("www.mikebobadilla.com");
+    db.setDatabaseName("cs441");
+    db.setUserName("cs441");
+    db.setPassword("HwhW4QWYHjV7tTkX");
+    bool ok = db.open();
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
