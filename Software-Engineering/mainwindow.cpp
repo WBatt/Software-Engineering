d817ae8f8faf84fdeff440868201b10e52a14917
Will <willabatt@gmail.com>
2017-02-14 18:28:04 -0800

    Login page set up, Button navigation

--- Software-Engineering/mainwindow.cpp
+++ Software-Engineering/mainwindow.cpp
@@ -17,3 +17,41 @@ void MainWindow::on_pushButton_clicked()
 {
     ui->stackedWidget->setCurrentIndex(1);
 }
+
+
+
+
+void MainWindow::on_pushButton_Login_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_CreateAccount_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_pushButton_GOTO_Login_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_GOTO_CreateAccount_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_pushButton_ForgotPass_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_register_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_CancelCreateAccount_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
