9f233770dfdb4d820899f2098b10e0706ac7a4a1
Shane Sharareh <shane.s.sharareh@gmail.com>
2017-03-06 18:31:05 -0800

    Updated user functions

--- api/server.js
+++ api/server.js
@@ -80,19 +80,31 @@ apiRouter.route('/users')
 			//duplicate entry
 			if(err.code == 11000)
 				return res.json({success:false, message: 'A user with that username already exists.'});
-				
+
 				else
 					return res.send(err);
 				}
-			
+
 
 				res.json({success: true, message: 'User created!'});
 			});
+})
+
+
+//get all the users(accessed at GET http://localhost:8080/api/users)
+.get(function(req, res){
+
+		User.find(function(err, users){
+			if(err) res.send(err);
+			//return the users
+			res.json(users);
+		});
 });
 
 
+
 apiRouter.post('/login', function(req, res){
-	
+
 	var query = User.findOne({username: req.body.username});
 	query.select("username password token");
 	var promise = query.exec();
@@ -131,8 +143,60 @@ apiRouter.post('/login', function(req, res){
 		});
 	});
 });
-				
 
+apiRouter.route('/users/:user_id')
+		//get the user with that id
+		//(accessed at GET http://localhost:8080/api/users/:user_id)
+.get(function(req, res){
+			User.findById(req.params.user_id, function(err,user){
+				if(err)
+					res.send(err);
+
+				//return that user
+				res.json(user);
+			});
+		})
+		//update the user with this id
+		//(accessed at PUT http://localhost:8080/api/users/:user_id)
+		.put(function(req, res){
+
+			//use our model to find the user we want
+			User.findById(req.params.user_id, function(err,user){
+				if(err)
+					res.json({"err": err});
+
+				//update the user info only if its new
+				if(req.body.name)
+					user.name = req.body.name;
+				if(req.body.username)
+					user.username = req.body.username;
+				if(req.body.password)
+					user.password = req.body.password;
+				//save the user
+				user.token_expiration = new Date();
+				user.save(function(err){
+					if(err)
+						res.json({"err": err});
+					else
+					//return a message
+						res.json({message: 'User updated!'});
+				});
+
+	});
+})
+
+//delete the user with this id
+//(accessed at DELETE http://localhost:8080/api/user/:user_id)
+.delete(function(req,res) {
+	User.remove({
+		_id: req.params.user_id
+	}, function(err,user){
+				if(err)
+					return res.json({"err": err});
+
+				res.json({message: 'Successfully deleted'});
+	});
+});
 //REGISTER OUR ROUTES -------------------
 //all of our routes will be prefixed with /api
 app.use('/api',apiRouter);
