43e11497ff1a1b8a3c7d41c15d175cfa6eda193d
Shane Sharareh <shane.s.sharareh@gmail.com>
2017-03-07 17:00:27 -0800

    created item.js

---
+++ app/models/item.js
@@ -0,0 +1,13 @@
+//grab PACKAGES
+var  mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//item Schema
+var ItemSchema = new Schema({
+  name: {type: String, required: true},
+  Description: String,
+  exp_date: {type: Date, required: true }
+});
+
+//return model
+module.exports = mongoose.model('Item', ItemSchema);
