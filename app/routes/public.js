7b567a105d892d741512f5f56b7c10e8e1b89dfc
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2017-04-13 13:15:22 -0700

    formatting

--- app/routes/public.js
+++ app/routes/public.js
@@ -1,34 +1,45 @@
-var path = require("path")
-module.exports = function(app, express, passport){
+var path = require("path");
+module.exports = function(app, express, passport) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/app/views", "index.html"));
+  });
 
-	app.get('/',function(req,res){
-		res.sendFile(path.join(__dirname, '../../public/app/views', 'index.html'));
-	});
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/app/views", "login.html"));
+  });
 
-	app.get('/login',function(req,res){
-		res.sendFile(path.join(__dirname, '../../public/app/views', 'login.html'));
-	});
+  app.get("/profile", function(req, res) {
+    res.sendFile(
+      path.join(__dirname, "../../public/app/views", "profile.html")
+    );
+  });
 
-	app.get('/profile',function(req,res){
-		res.sendFile(path.join(__dirname, '../../public/app/views', 'profile.html'));
-	});
+  app.get("/dashboard", function(req, res) {
+    res.sendFile(
+      path.join(__dirname, "../../public/app/views", "dashboard.html")
+    );
+  });
 
-	app.get('/dashboard',function(req,res){
-		res.sendFile(path.join(__dirname, '../../public/app/views', 'dashboard.html'));
-	});
+  app.get("/createAccount", function(req, res) {
+    res.sendFile(
+      path.join(__dirname, "../../public/app/views", "createAccount.html")
+    );
+  });
 
-	app.get('/createAccount',function(req,res){
-		res.sendFile(path.join(__dirname, '../../public/app/views', 'createAccount.html'));
-	});
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/app/views", "about.html"));
+  });
 
-	app.get('/about',function(req,res){
-		res.sendFile(path.join(__dirname, '../../public/app/views', 'about.html'));
-	});
-
-
-	// facebook route
-	app.get('/auth/facebook', passport.authenticate('facebook', {failureRedirect: '/login'}))
-	app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/login'}), function(req, res){
-		res.redirect('/')
-	})
+  // facebook route
+  app.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", { failureRedirect: "/login" })
+  );
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    function(req, res) {
+      res.redirect("/");
+    }
+  );
 };
--- server.js
+++ server.js
@@ -2,53 +2,60 @@
 //==================
 
 //CALL THE PACKAGES--------------
-var express			= require('express');		//call express
-var session			= require('express-session');
-var app				= express();				//define our app using express
-var bodyParser 		= require('body-parser');	// get body-parser
-var fs 				= require('fs');
-var path 			= require('path');
-var morgan			= require('morgan');		// used to see requests
-var mongoose		= require('mongoose');		// for worki  ng w/ our database
-var passport 		= require('passport');
-mongoose.Promise 	= require('bluebird');
+var express = require("express"); //call express
+var session = require("express-session");
+var app = express(); //define our app using express
+var bodyParser = require("body-parser"); // get body-parser
+var fs = require("fs");
+var path = require("path");
+var morgan = require("morgan"); // used to see requests
+var mongoose = require("mongoose"); // for worki  ng w/ our database
+var passport = require("passport");
+mongoose.Promise = require("bluebird");
 
-var config 			= require('./config');
-var port			= config.port;				//set the port for our app
+var config = require("./config");
+var port = config.port; //set the port for our app
 
 // APP CONFIGURATION -------------
 // use body parser so we can grab information from POST requests
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public/app'));
-app.use(session({
-	secret: 'foooooood',
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(express.static(__dirname + "/public/app"));
+app.use(
+  session({
+    secret: "foooooood",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-require('./config/passport.js')(passport);
+require("./config/passport.js")(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
 //configure our app to handle CORS requests
-app.use(function(req,res,next){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods','GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Request-With,content-type, \ Authorization');
-	next();
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Request-With,content-type, \ Authorization"
+  );
+  next();
 });
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a"
+});
 
-app.use(morgan('combined', {stream: accessLogStream}));
+app.use(morgan("combined", { stream: accessLogStream }));
 
 //ROUTES FOR OUR API
 //============================
 
 //get an instance of the express router
-var apiRoutes  = require('./app/routes/api')(app, express, passport);
-require('./app/routes/public')(app, express, passport);
+var apiRoutes = require("./app/routes/api")(app, express, passport);
+require("./app/routes/public")(app, express, passport);
 
 // Main catchall route
 //app.get("*", function(req, res){
@@ -57,11 +64,11 @@ require('./app/routes/public')(app, express, passport);
 
 //REGISTER OUR ROUTES -------------------
 //all of our routes will be prefixed with /api
-app.use('/api',apiRoutes);
+app.use("/api", apiRoutes);
 
 //START THE SERVER
 //==========================================
 app.listen(port);
-console.log("app is on localhost:" + port)
+console.log("app is on localhost:" + port);
 //connect to our database (hosted on modulus.io)
 mongoose.connect(config.database);
